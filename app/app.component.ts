9516eba0aa9cfff1702e0e04fbd794bfbde77648
Sathishchary <ksathish@xtremand.com>
2019-03-24 11:54:55 -0600

    2 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,25 +5,38 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  startDate = '03-24-2019';
+  startDate = '2019-03-24';
   arrayDates = [];
   isoDate:any;
   isoDates = [];
-  constructor(){ }
+  currentDate =  new Date();
+  constructor(){
+
+   }
   
   storeDays(){
     let dates:any = [];
     for(let i=0; i< 7; i++){
-     dates.push(this.storeDates(this.startDate, i));
+     let date = new Date(this.storeDates(this.startDate, i));
+     let datesv:any = date.toISOString();
+     datesv = datesv.split('T');
+     datesv = datesv[0];
+     dates.push(datesv);
     }
     this.isoDates = dates;
     console.log(dates)
+     for(let i=0; i< this.isoDates.length; i++){
+       const value = new Date(this.isoDates[i])
+       console.log(value.toISOString());
+    }
   }
    storeDates(startDateformatChange, i){
      const date = new Date(startDateformatChange);
-    // const isoDate = date.setDate(date.getDate() + i);
-     const isoDate =  date.setUTCDate(date.getUTCDate() + i);
-    return isoDate.toString();
+    //  const isoDate = date.setDate(date.getDate() + i);
+     let isoDate =  date.setUTCDate(date.getUTCDate() + i);
+    // const d = new Date(startDateformatChange); 
+    //  const datev =  d.setMinutes(d.getMinutes() + d.getTimezoneOffset());
+    return isoDate;
   }
   ngOnInit(){
      this.storeDays();
